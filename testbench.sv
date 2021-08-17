7a941f0c33d71ec82b481351d82f54d0fa4924ed
Eric Souza <eric.bh18souza@gmail.com>
2021-08-17 18:07:33 -0300

    First commit

---
+++ testbench.sv
@@ -0,0 +1,20 @@
+module jc_tb;
+  reg clock, reset;
+  wire [3:0] out;
+  
+  johnson_counter dut (.out(out), .reset(reset), .clock(clock));
+  
+  always 
+    #5 clock = ~clock;
+  
+  initial begin
+    reset = 1'b1; 
+    clock = 1'b0;
+    #20 reset= 1'b0;
+  end
+  
+  initial begin 
+    $monitor($time, "clock=%b, out= %b, reset=%b", clock, out, reset);
+    #105 $stop;  
+  end
+endmodule
